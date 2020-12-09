54aee3705a91d5c9babbf4567b8332289ba44665
Uladzislau Pratasevich <protosevich2001@gmail.com>
2020-12-09 23:15:04 +0300

    feat: splitting into projects

--- Model/Properties/Resources.Designer.cs
+++ Model/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Transport.Properties {
+namespace Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Transport.Properties {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Transport.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Model.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
